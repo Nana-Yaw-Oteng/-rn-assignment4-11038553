2d68c04f2dd478f60da0de25da1a7b0959318a20
Nana-Yaw-Oteng <nyooteng@st.ug.edu.gh>
2024-06-14 12:41:11 +0000

    Added Greetings component

---
+++ Shoppie/App.js
@@ -0,0 +1,10 @@
+import {  Text, View } from 'react-native';
+import Greetings from './components/Greetings';
+
+export default function App() {
+  return (
+    <View>
+     <Greetings/>
+    </View>
+  );
+}
